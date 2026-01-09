539a3777da0e30f99ea1bce9308eeac8f414ccbc
Tovhea <thubinette@hotmail.com>
2026-01-09 12:49:02 +0100

    databasen funkar, filen gik889.db tillagd

--- server/flowers.sql
+++ server/flowers.sql
@@ -1,19 +1,18 @@
-
-
-
 DROP TABLE IF EXISTS flowers; --Tar bort 
 CREATE TABLE IF NOT EXISTS flowers(
-  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  id          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 
-  name VARCHAR(8) NOT NULL,
-  petalShape VARCHAR(9) NOT NULL,
-  width INTEGER NOT NULL,
-  color VARCHAR(6) NOT NULL,
+  name        VARCHAR(8) NOT NULL,
+  petalShape  VARCHAR(9) NOT NULL,
+  width       INTEGER NOT NULL,
+  color       VARCHAR(6) NOT NULL
 );
 
-INSERT INTO flowers(id,name,petalshape,width,color) VALUES (1,'Zena','Zulauf','Katlynn_Brekke','green'); 
+INSERT INTO flowers(id,name,petalShape,width,color) 
+VALUES (1,'Zena','Zulauf',2,'lila'); 
 
-INSERT INTO flowers(id,name,petalShape,width,color) VALUES (1,'Dahlia','Runda','2','Pink');
+INSERT INTO flowers(id,name,petalShape,width,color) 
+VALUES (2,'Dahlia','Runda',2,'rosa');
 
 /*
 INSERT INTO users(id,firstName,lastName,username,color) VALUES (2,'Muhammad','Torphy','Martina39','gray');

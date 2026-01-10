38a7f51fa0f84f128f6dc097561ee3b992792572
DayTime-ping <maikkarlsson02@gmail.com>
2026-01-10 22:36:57 +0100

    Add flower.sql

---
+++ server/flower.sql
@@ -0,0 +1,35 @@
+-- SQLite
+-- SQLite
+DROP TABLE IF EXISTS flowers; --Tar bort 
+CREATE TABLE IF NOT EXISTS flowers(
+  id          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+
+  name        VARCHAR(8) NOT NULL,
+  petalShape  VARCHAR(9) NOT NULL,
+  width       INTEGER NOT NULL,
+  color       VARCHAR(6) NOT NULL
+);
+
+INSERT INTO flowers(id,name,petalShape,width,color) 
+VALUES (1,'Zena','Zulauf',2,'lila'); 
+INSERT INTO flowers(id,name,petalShape,width,color) 
+VALUES (2,'Dahlia','Runda',2,'rosa');
+INSERT INTO flowers(id,name,petalShape,width,color) 
+VALUES (3,'Tulpan','Koppad','5','orange');
+INSERT INTO flowers(id,name,petalShape,width,color) 
+VALUES (4,'Vallmo','Bågad','7','röd');
+INSERT INTO flowers(id,name,petalShape,width,color) 
+VALUES (5,'Solros','Triangulära','10','gul');
+
+/*
+INSERT INTO users(id,firstName,lastName,username,color) VALUES (3,'Carlee','Tromp','Carmen37','purple');
+INSERT INTO users(id,firstName,lastName,username,color) VALUES (4,'Taylor','Shanahan','Doyle_Legros81','red');
+INSERT INTO users(id,firstName,lastName,username,color) VALUES (5,'Estell','Reichel','Santiago.Dibbert','red');
+INSERT INTO users(id,firstName,lastName,username,color) VALUES (6,'Reece','Stehr','Destany75','red');
+INSERT INTO users(id,firstName,lastName,username,color) VALUES (7,'Kiarra','Beier','Edison87','yellow');
+INSERT INTO users(id,firstName,lastName,username,color) VALUES (8,'Alberto','Gibson','Marianna_Collins','green');
+INSERT INTO users(id,firstName,lastName,username,color) VALUES (9,'Johanna','Bashirian','Mervin.Grant','yellow');
+INSERT INTO users(id,firstName,lastName,username,color) VALUES (10,'Thalia','Kozey','Ashley22','yellow');*/
+
+
+select * from flowers; 

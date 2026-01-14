afcffb7c4a10e358e975af7facdf1ab45d0ca44c
DayTime-ping <maikkarlsson02@gmail.com>
2026-01-14 13:56:40 +0100

    Tutorial borta

--- server/flowers.sql
+++ server/flowers.sql
@@ -21,15 +21,7 @@ VALUES (4,'Vallmo','Bågad',7,'röd');
 INSERT INTO flowers(id,name,petalShape,width,color) 
 VALUES (5,'Solros','Triangulära',10,'gul');
 
-/*
-INSERT INTO users(id,firstName,lastName,username,color) VALUES (3,'Carlee','Tromp','Carmen37','purple');
-INSERT INTO users(id,firstName,lastName,username,color) VALUES (4,'Taylor','Shanahan','Doyle_Legros81','red');
-INSERT INTO users(id,firstName,lastName,username,color) VALUES (5,'Estell','Reichel','Santiago.Dibbert','red');
-INSERT INTO users(id,firstName,lastName,username,color) VALUES (6,'Reece','Stehr','Destany75','red');
-INSERT INTO users(id,firstName,lastName,username,color) VALUES (7,'Kiarra','Beier','Edison87','yellow');
-INSERT INTO users(id,firstName,lastName,username,color) VALUES (8,'Alberto','Gibson','Marianna_Collins','green');
-INSERT INTO users(id,firstName,lastName,username,color) VALUES (9,'Johanna','Bashirian','Mervin.Grant','yellow');
-INSERT INTO users(id,firstName,lastName,username,color) VALUES (10,'Thalia','Kozey','Ashley22','yellow');*/
+
 
 
 select * from flowers; 
